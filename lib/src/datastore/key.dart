1c47c7c768ccfbd4cc43f26377e66d22ab5c2e23
Peter Siroki <psiroki@skawa.hu>
2017-11-13 11:36:57 +0100

    Added a null check for kind in Key

--- lib/src/datastore/key.dart
+++ lib/src/datastore/key.dart
@@ -11,6 +11,7 @@ class Key implements ApiRepresentation<ds.Key> {
   /// Creates a key of [kind] with a [parent] key as incomplete or either with
   /// an [id] or a [name] (at most one can be specified of these).
   Key(this.kind, {this.name, this.id, this.parent}) {
+    if (kind == null) throw new DatastoreShellError("The kind must not be null");
     if (parent != null && !parent.isComplete)
       throw new DatastoreShellError("Parent key is not complete");
     if (name != null && id != null)
