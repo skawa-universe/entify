5665575b184c08d09c4fde996db664d7039f8bf7
Peter Siroki <psiroki@skawa.hu>
2021-05-27 13:11:06 +0200

    Added isWriteOnlyProperty to EntityBridge

--- lib/src/entity/errors.dart
+++ lib/src/entity/errors.dart
@@ -1,11 +1,24 @@
+class EntifyException implements Exception {
+
+}
+
 /// Thrown when there was an error during entity
 /// model metadata extraction or during entity
 /// property mapping.
-class EntityModelError extends Error {
-  EntityModelError(this.message);
+class EntityModelException extends EntifyException {
+  EntityModelException(this.message);
 
   @override
   String toString() => "EntityModelError: $message";
 
   final String message;
+}
+
+class PropertyNotFoundException extends EntifyException {
+  PropertyNotFoundException(this.propertyName);
+
+  @override
+  String toString() => "PropertyNotFoundException: $propertyName";
+
+  final String propertyName;
 }
